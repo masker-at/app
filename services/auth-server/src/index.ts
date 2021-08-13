318602a50e64e75ec0db263dbb27bfc3e39829fd
antonlyap <laposhchenkoanton@gmail.com>
2021-08-13 16:45:00 +0300

    Set up Fastify

--- services/auth-server/src/index.ts
+++ services/auth-server/src/index.ts
@@ -1,16 +1,22 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { User } from '@masker-at/postgres-models';
+import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
 
 (async () => {
   const connection = await createConnection();
   await connection.runMigrations();
-  console.log(connection.options);
-  console.log(await User.find());
-  await User.create({
-    email: 'hello@hello.com',
-    passwordHash: 'hash',
-  }).save();
+
+  const app = fastify();
+  await app.register(fastifyCors);
+
+  app.get('/ping', async (req, res) => {
+    await res.send('pong');
+  });
+
+  await app.listen(3000, '0.0.0.0');
+  console.log('Listening on port 3000');
 })().catch((err) => {
   console.error(err);
   process.exit(1);
