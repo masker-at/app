68cab532a922b178750defa592b3b9f54b6de3e4
antonlyap <laposhchenkoanton@gmail.com>
2021-08-20 10:25:51 +0300

    Add buttons to the dashboard sidebar

--- frontend/web/src/components/dashboard/Sidebar.tsx
+++ frontend/web/src/components/dashboard/Sidebar.tsx
@@ -3,10 +3,42 @@ import { FC } from 'react';
 const Sidebar: FC = () => (
   <aside className="font-quicksand flex-shrink-0 w-44 pr-5 border-r border-gray-200">
     <ul>
-      <li className="my-1 rounded px-3 py-1 bg-primary bg-opacity-25 text-primary-darker font-bold">
-        My Aliases
+      <li>
+        <button
+          type="button"
+          className="
+            my-1
+            rounded
+            w-full
+            px-3
+            py-1
+            text-left
+            hover:bg-primary
+            hover:bg-opacity-25
+            hover:text-primary-darker
+          "
+        >
+          My Aliases
+        </button>
       </li>
-      <li className="my-1 rounded px-3 py-1">Account Settings</li>
+      <li>
+        <button
+          type="button"
+          className="
+            my-1
+            rounded
+            w-full
+            px-3
+            py-1
+            text-left
+            hover:bg-primary
+            hover:bg-opacity-25
+            hover:text-primary-darker
+          "
+        >
+          Account Settings
+        </button>
+      </li>
     </ul>
   </aside>
 );
