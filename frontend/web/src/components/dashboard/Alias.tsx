5e1097b3d6002d912e0eebe5aa503ba85d748f6c
antonlyap <laposhchenkoanton@gmail.com>
2021-08-20 22:46:07 +0300

    Add a "Copy" button for each alias

--- frontend/web/src/components/dashboard/Alias.tsx
+++ frontend/web/src/components/dashboard/Alias.tsx
@@ -24,6 +24,8 @@ const Alias: FC<Props> = ({ alias }) => {
 
   const finishAliasCreation = useFinishAliasCreation();
 
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
     <div className="rounded border border-primary bg-gray-100 p-2 mb-3">
       {isEditing || alias.isNew ? (
@@ -86,9 +88,18 @@ const Alias: FC<Props> = ({ alias }) => {
           </button>
         </p>
       )}
-      <a className="font-quicksand text-primary-darker" href={`mailto:${alias.address}`}>
-        {alias.address}
-      </a>
+      <span className="font-quicksand text-primary-darker">{alias.address}</span>{' '}
+      <button
+        type="button"
+        className="text-primary-darker underline text-sm"
+        onClick={async () => {
+          await navigator.clipboard.writeText(alias.address);
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 2000);
+        }}
+      >
+        {isCopied ? 'Copied!' : 'Copy'}
+      </button>
     </div>
   );
 };
