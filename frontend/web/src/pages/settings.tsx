07c9b13533a5613ec5f18712cf3e786ec6ba2d4d
antonlyap <laposhchenkoanton@gmail.com>
2021-08-23 12:08:13 +0300

    Add UI to change email and password (not connected to API)

--- frontend/web/src/pages/settings.tsx
+++ frontend/web/src/pages/settings.tsx
@@ -7,6 +7,7 @@ import Cookies from 'cookies';
 import me from '../utils/api/me';
 import Header from '../components/dashboard/Header';
 import Sidebar from '../components/dashboard/Sidebar';
+import Input from '../components/Input';
 
 const SettingsPage: FC = () => (
   <div className="container mx-auto px-10 overflow-auto min-h-screen flex flex-col">
@@ -17,7 +18,39 @@ const SettingsPage: FC = () => (
     <Header />
     <div className="flex-1 flex flex-col sm:flex-row">
       <Sidebar />
-      
+      <main className="flex-grow sm:pl-5 xl:mr-72 flex flex-col">
+        <section className="my-2">
+          <h1 className="text-2xl font-quicksand">Email</h1>
+          <p className="text-sm mt-1 mb-2">
+            We will forward incoming emails from your aliases to this address, as well as send you
+            system emails
+          </p>
+          <form>
+            <div className="flex flex-col w-64">
+              <Input placeholder="Email..." aria-label="Email" />
+              <button type="submit" className="button bg-primary active:bg-primary-darker mt-2">
+                Save
+              </button>
+            </div>
+            <p className="text-sm text-gray-500">You will need to verify your new email</p>
+          </form>
+        </section>
+
+        <section className="my-2">
+          <h1 className="text-2xl font-quicksand">Password</h1>
+          <p className="text-sm mt-1 mb-2">You can change the password of your account here</p>
+          <form className="flex flex-col w-64 items-stretch">
+            <Input placeholder="Current password..." aria-label="Current password" />
+            <div aria-hidden className="h-2" />
+            <Input placeholder="New password..." aria-label="New password" />
+            <div aria-hidden className="h-2" />
+            <Input placeholder="Repeat new password..." aria-label="Repeat new password" />
+            <button type="submit" className="button bg-primary active:bg-primary-darker mt-2">
+              Save
+            </button>
+          </form>
+        </section>
+      </main>
     </div>
   </div>
 );
