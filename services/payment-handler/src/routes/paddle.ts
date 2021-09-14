8862edeb0a5fa6e03a40057c4e35227481d9adf4
antonlyap <laposhchenkoanton@gmail.com>
2021-09-14 21:58:18 +0300

    Handle the `subscription_created` webhook from Paddle

--- services/payment-handler/src/routes/paddle.ts
+++ services/payment-handler/src/routes/paddle.ts
@@ -1,9 +1,21 @@
 import { FastifyInstance } from 'fastify';
+import { User } from '@masker-at/postgres-models';
 import verifyWebhook from '../hooks/verifyWebhook';
 
 export default async function paddleRoute(app: FastifyInstance): Promise<void> {
-  app.post('/paddle', { preHandler: verifyWebhook }, async (req, res) => {
-    console.log(req.body);
+  app.post<{ Body: any }>('/paddle', { preHandler: verifyWebhook }, async (req, res) => {
+    switch (req.body.alert_name) {
+      case 'subscription_created': {
+        const { userID } = req.body.passthrough;
+        const user = await User.findOne({ id: userID });
+        if (!user) break;
+
+        user.paddleID = req.body.subscription_id;
+        await user.save();
+        break;
+      }
+      default:
+    }
     await res.send('OK');
   });
 }
