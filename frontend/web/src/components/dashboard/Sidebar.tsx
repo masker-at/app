33ab9326ed1d65a1084529b5b91a8669aa3d38c0
antonlyap <laposhchenkoanton@gmail.com>
2021-09-25 18:36:53 +0300

    Show the billing link only if email is verified

--- frontend/web/src/components/dashboard/Sidebar.tsx
+++ frontend/web/src/components/dashboard/Sidebar.tsx
@@ -2,12 +2,15 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
+import useMeQuery from '../../api-hooks/useMeQuery';
 
 const Sidebar: FC = () => {
   const router = useRouter();
   const isDashboard = router.pathname.includes('/dashboard');
   const isSettings = router.pathname.includes('/settings');
   const isBilling = router.pathname.includes('/billing');
+
+  const { data } = useMeQuery();
 
   return (
     <aside className="font-quicksand flex-shrink-0 sm:w-44 sm:pr-5 sm:border-r border-gray-200 mb-3 sm:mb-0">
@@ -64,32 +67,34 @@ const Sidebar: FC = () => {
             </a>
           </Link>
         </li>
-        <li>
-          <Link href="/billing">
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a
-              className={clsx(
-                `
-                  block
-                  my-1
-                  rounded
-                  w-full
-                  px-3
-                  py-1
-                  text-left
-                  hover:bg-primary
-                  hover:bg-opacity-25
-                  hover:text-primary-darker
-                `,
-                {
-                  'bg-primary bg-opacity-25 text-primary-darker font-bold': isBilling,
-                },
-              )}
-            >
-              Billing
-            </a>
-          </Link>
-        </li>
+        {data!.isEmailVerified && (
+          <li>
+            <Link href="/billing">
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a
+                className={clsx(
+                  `
+                    block
+                    my-1
+                    rounded
+                    w-full
+                    px-3
+                    py-1
+                    text-left
+                    hover:bg-primary
+                    hover:bg-opacity-25
+                    hover:text-primary-darker
+                  `,
+                  {
+                    'bg-primary bg-opacity-25 text-primary-darker font-bold': isBilling,
+                  },
+                )}
+              >
+                Billing
+              </a>
+            </Link>
+          </li>
+        )}
       </ul>
     </aside>
   );
