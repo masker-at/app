b2c896e0b66284a04fe0f721555e635506a2f429
antonlyap <laposhchenkoanton@gmail.com>
2021-10-07 15:55:20 +0300

    Rename the API_BASE_URL env variable

--- frontend/web/src/utils/api/client.ts
+++ frontend/web/src/utils/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
   xsrfCookieName: 'ct',
   xsrfHeaderName: 'X-CSRF-Token',
   withCredentials: true,
