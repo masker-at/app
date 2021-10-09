34b54417ebc728baa60edbede4c74ba6b99e73a0
antonlyap <laposhchenkoanton@gmail.com>
2021-10-09 17:39:37 +0300

    Add the API_PRIVATE_URL env variable to the frontend for server-to-server requests

--- frontend/web/src/utils/api/client.ts
+++ frontend/web/src/utils/api/client.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+  baseURL:
+    typeof window === 'undefined'
+      ? process.env.API_PRIVATE_URL ||
+        process.env.NEXT_PUBLIC_API_BASE_URL ||
+        'http://localhost:3000'
+      : process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
   xsrfCookieName: 'ct',
   xsrfHeaderName: 'X-CSRF-Token',
   withCredentials: true,
