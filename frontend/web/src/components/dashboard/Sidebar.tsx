98361ab84975115558f6e2d362ddcc8247ab71e7
antonlyap <laposhchenkoanton@gmail.com>
2021-10-12 22:10:00 +0300

    Add the link to extension stores

--- frontend/web/src/components/dashboard/Sidebar.tsx
+++ frontend/web/src/components/dashboard/Sidebar.tsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import Bowser from 'bowser';
 import useMeQuery from '../../api-hooks/useMeQuery';
 
 const Sidebar: FC = () => {
@@ -11,6 +12,31 @@ const Sidebar: FC = () => {
   const isBilling = router.pathname.includes('/billing');
 
   const { data } = useMeQuery();
+
+  const [storeLink, setStoreLink] = useState<string | null>(null);
+  useEffect(() => {
+    const browserName = Bowser.getParser(navigator.userAgent).getBrowserName();
+    switch (browserName) {
+      case 'Opera':
+        setStoreLink('https://addons.opera.com/extensions/details/masker/');
+        break;
+      case 'Firefox':
+        setStoreLink('https://addons.mozilla.org/firefox/addon/masker/');
+        break;
+      case 'Chrome':
+      case 'NAVER Whale Browser':
+      case 'Yandex Browser':
+      case 'UC Browser':
+      case 'Maxthon':
+      case 'Microsoft Edge':
+      case 'Vivaldi':
+      case 'Chromium':
+      case 'Google Search':
+        setStoreLink('https://masker.at'); // TODO: Change
+        break;
+      default:
+    }
+  }, []);
 
   return (
     <aside className="font-quicksand flex-shrink-0 sm:w-44 sm:pr-5 sm:border-r border-gray-200 mb-3 sm:mb-0">
@@ -91,6 +117,30 @@ const Sidebar: FC = () => {
                 )}
               >
                 Billing
+              </a>
+            </Link>
+          </li>
+        )}
+        {storeLink && (
+          <li>
+            <Link href={storeLink}>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a
+                className={clsx(
+                  `
+                  block
+                  my-1
+                  rounded
+                  w-full
+                  px-3
+                  py-1
+                  text-left
+                  text-primary-darker
+                  underline
+                `,
+                )}
+              >
+                Get our extension
               </a>
             </Link>
           </li>
